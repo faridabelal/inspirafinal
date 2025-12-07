d4bc9c4e8ce49da96d68c74666df4411371413e1
faridabelal <146150469+faridabelal@users.noreply.github.com>
2025-12-07 15:55:58 -0600

    Update db.php

--- Inspira2/includes/db.php
+++ Inspira2/includes/db.php
@@ -1,13 +1,22 @@
 <?php
-$host = "localhost";
-$dbname = "febelal";
-$username = "febelal";   // or your MySQL username
-$password = "oleMissF25";       // your MySQL password
+$host = "mysql-199333d6-faridabelal2004-bcaa.g.aivencloud.com";
+$port = "22124";
+$dbname = "defaultdb";
+$username = "avnadmin";   // or your MySQL username
+$password = "AVNS_ZF6XkN5Zd-Gkg7IGTYc";       // your MySQL password
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
 } catch (PDOException $e) {
-    die("DB Connection failed: " . $e->getMessage());
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
